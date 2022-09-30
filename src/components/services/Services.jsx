1ca08223aac841c25a79d790e2a5a36db1a5bfc5
devmarcioreis <107413382+devmarcioreis@users.noreply.github.com>
2022-09-30 14:54:00 -0300

    Desenvolvendo section services

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,10 +1,110 @@
 import React from 'react'
 import './services.css'
+import { GiCheckMark } from 'react-icons/gi'
 
 const Services = () => {
   return (
 
-    <section id='services'>Meus serviços</section>
+    <section id='services'>
+      <h5>Veja os</h5>
+      <h2>Meus serviços</h2>
+
+      <div className="container services__container">
+        <article className='service'>
+          <div className="service__head">
+            <h3>Front-end</h3>
+          </div>
+          <ul className='service__list'>
+            <li>
+              < GiCheckMark className='service__list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              < GiCheckMark className='service__list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              < GiCheckMark className='service__list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              < GiCheckMark className='service__list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              < GiCheckMark className='service__list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+          </ul>
+        </article>
+        <article className='service'>
+          <div className="service__head">
+            <h3>Full-stack</h3>
+          </div>
+          <ul className='service__list'>
+            <li>
+              < GiCheckMark className='service__list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              < GiCheckMark className='service__list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              < GiCheckMark className='service__list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              < GiCheckMark className='service__list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              < GiCheckMark className='service__list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              < GiCheckMark className='service__list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              < GiCheckMark className='service__list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              < GiCheckMark className='service__list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+          </ul>
+        </article>
+        <article className='service'>
+          <div className="service__head">
+            <h3>Back-end</h3>
+          </div>
+          <ul className='service__list'>
+            <li>
+              < GiCheckMark className='service__list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              < GiCheckMark className='service__list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              < GiCheckMark className='service__list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              < GiCheckMark className='service__list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              < GiCheckMark className='service__list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+          </ul>
+        </article>
+      </div>
+    </section>
 
   )
 }
